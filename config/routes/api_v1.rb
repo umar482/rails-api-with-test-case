7f8d774db4bce53fe5ac5c875e4a335a7f53f234
umar482 <umar.omtomo@gmail.com>
2022-05-26 19:49:30 +0500

    Add api routes for resources

--- config/routes/api_v1.rb
+++ config/routes/api_v1.rb
@@ -1,6 +1,8 @@
 namespace :api do
   namespace :v1, defaults: { format: :json } do
     get :status, to: 'api#status'
-    # resources :reservations #, only: %i[create, update]
+    resources :users
+    resources :gears
+    resources :temperature_readings
   end
 end
