13299c312a2681ccee2568a2cec15f5bd892a259
umar482 <umar.omtomo@gmail.com>
2022-05-29 18:09:28 +0500

    Refactor code

--- app/controllers/api/v1/temperature_readings_controller.rb
+++ app/controllers/api/v1/temperature_readings_controller.rb
@@ -3,10 +3,10 @@
 module Api
   module V1
     class TemperatureReadingsController < ApiController
-      
       before_action :set_actual_temp, only: [:create]
 
       private
+
       def resource_params
         params.require(:temperature_reading).permit(
           :user_id, :body_temp, :actual_temp
@@ -15,11 +15,10 @@ module Api
 
       def set_actual_temp
         @user = User.find_by(id: params[:temperature_reading][:user_id])
-        debugger
-        if @user
-          params[:temperature_reading][:actual_temp] = params[:temperature_reading][:body_temp] + @user.gear.offset_value # temperature caliberation
-        end
-        debugger
+        return if @user.blank? || params[:body_temp].nil?
+
+        # temperature caliberation
+        params[:temperature_reading][:actual_temp] = params[:temperature_reading][:body_temp] + @user.gear.offset_value
       end
     end
   end
